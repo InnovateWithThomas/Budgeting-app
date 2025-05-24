21318d2707a9e953a118cfacbf5c53f2f026c0dd
Thomas PAZZÉ <thomas.pazze@gmail.com>
2025-05-24 20:23:09 +0200

    feat: auth0 setup

--- api/Budgeting App/Program.cs
+++ api/Budgeting App/Program.cs
@@ -1,6 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
+using Budgeting_App;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using RestSharp;
+using System.Security.Claims;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+var domain = builder.Configuration["Auth0:Domain"];
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"https://{domain}/";
+        options.Audience = builder.Configuration["Auth0:Audience"];
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = ClaimTypes.NameIdentifier
+        };
+    });
+builder.Services
+    .AddAuthorization(options =>
+    {
+        options.AddPolicy(
+            "read:messages",
+            policy => policy.Requirements.Add(
+                new HasScopeRequirement("read:messages", domain)
+                )
+            );
+    });
+
+builder.Services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +36,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +51,4 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
---
+++ api/Budgeting App/TestAuthService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.OpenApi.Models;
+using RestSharp;
+
+namespace Budgeting_App
+{
+    public static class TestAuthService
+    {
+        public static void GetAuth0Token()
+        {
+            Console.WriteLine("jon doejon doejon doejon doejon doe");
+            var client = new RestClient("https://localhost:7244/WeatherForecast");
+            var request = new RestRequest(RestSharp.Method.Get.ToString());
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\"client_id\":\"CLMgzUANnetf47g9JeXRCe6kGxPuY8cr\",\"client_secret\":\"F17TnuRdmwtorI36Sg7xLxiYc5kk-gHTUZbgLycLrWuXwgNx-BdLPtLm8OBCEVYQ\",\"audience\":\"https://quickstarts/api\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+
+            Console.WriteLine("Result : "+response);
+        }
+    }
+}
