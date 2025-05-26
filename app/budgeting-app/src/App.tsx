48e7c60af6857b2de70fee70bc4a8d4e045e260f
Thomas PAZZÉ <thomas.pazze@gmail.com>
2025-05-26 12:40:18 +0200

    feat: add tailwinds css

---
+++ app/budgeting-app/src/App.tsx
@@ -0,0 +1,13 @@
+import LoginButton from "./components/auth/LoginButton";
+
+function App() {
+
+	return (
+    <div>
+			<h1 className="text-3xl font-bold underline">Hi you are connected</h1>
+			<LoginButton />
+    </div>
+  )
+}
+
+export default App
---
+++ app/budgeting-app/src/pages/registration.tsx
@@ -0,0 +1,12 @@
+import LoginButton from "../components/auth/LoginButton";
+
+function Registration() {
+	return (
+		<>
+			<div>sign in</div>
+			<LoginButton />
+		</>
+	)
+}
+
+export default Registration
